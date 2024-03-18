58782bca2de1965cab50df672f37a6233584ab4a
mk-github-1 <32920703+mk-github-1@users.noreply.github.com>
2024-03-18 12:48:29 +0900

    Update LoginUser.ts

--- backend/src/domain-model/entity/auth/LoginUser.ts
+++ backend/src/domain-model/entity/auth/LoginUser.ts
@@ -1,8 +1,13 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm'
-// import { LoginUserRole } from './LoginUserRole'; // リレーションエンティティのインポートが必要かもしれません
+import { LoginUserRole } from './LoginUserRole';
 
+/**
+ * LoginUser: ログインユーザー
+ *
+ */
+// @Table({ name: "login_user" })
 @Entity()
-export class User {
+export class LoginUser {
   constructor(
     username: string,
     password: string,
@@ -11,9 +16,10 @@ export class User {
     accountNonLocked: boolean,
     credentialsNonExpired: boolean,
     sortOrder: number,
-    isDeleted?: boolean,
-    createdAt?: Date,
-    updatedAt?: Date
+    isDeleted: boolean,
+    createdAt: Date,
+    updatedAt: Date,
+    loginUserRole: LoginUserRole[]
   ) {
     this.username = username
     this.password = password
@@ -27,45 +33,46 @@ export class User {
     // ■ 作成日時、更新日時の自動更新処理が必要
     this.createdAt = createdAt ?? new Date()
     this.updatedAt = updatedAt ?? new Date()
+
+    this.loginUserRoles = []
   }
 
-  // @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn({ name: 'username'})
   @Column({ length: 256 })
-  username: string
+  private username: string
 
   @Column({ length: 256, nullable: false })
-  password: string
+  private password: string
 
   // アカウントが有効かどうかを示すフラグ
   @Column({ default: true })
-  enabled: boolean
+  private enabled: boolean
 
   // アカウントの有効期限が切れているかどうかを示すフラグ
   @Column({ default: true })
-  accountNonExpired: boolean
+  private accountNonExpired: boolean
 
   // 資格情報の有効期限が切れているかどうかを示すフラグ
   @Column({ default: true })
-  accountNonLocked: boolean
+  private accountNonLocked: boolean
 
   // アカウントがロックされているかどうかを示すフラグ
   @Column({ default: true })
-  credentialsNonExpired: boolean
+  private credentialsNonExpired: boolean
 
   @Column()
-  sortOrder: number
+  private sortOrder?: number
 
   @Column({ nullable: true })
-  isDeleted?: boolean
+  private isDeleted: boolean
 
   @Column({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
-  createdAt: Date
+  private createdAt: Date
 
   @Column({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
-  updatedAt: Date
+  private updatedAt: Date
 
-  // ユーザーが持つ権限のリスト、DBからの取得用
-  // @OneToMany(mappedBy = "loginUser", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
-  // @OrderColumn
-  // loginUserRoles: LoginUserRole[]
+  // ユーザーが持つ権限のリスト
+  // @OneToMany({ mappedBy: "loginUser", cascade: CascadeType.ALL, orphanRemoval: true, fetch: FetchType.EAGER })
+  private loginUserRoles: LoginUserRole[]
 }
