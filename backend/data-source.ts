590ad4da08f718e0dc1a2efffe570808a4a88639
mk-github-1 <32920703+mk-github-1@users.noreply.github.com>
2024-03-21 12:55:33 +0900

    Update data-source.ts

--- backend/data-source.ts
+++ backend/data-source.ts
@@ -6,24 +6,35 @@ import { LoginUserRole } from './src/domain-model/entity/auth/LoginUserRole'
 import { Role } from './src/domain-model/entity/auth/Role'
 
 export const AppDataSource = new DataSource({
+  // 1.利用するデータベース
   type: 'sqlite', // or mysql or postgresql
   // host: "localhost"、sqliteは不要
   // port: "", // mysqlは3306、postresqlは5423、sqliteは不要
   // username: "", // 任意、sqliteは不要
   // password: "", // 任意、sqliteは不要
-  database: './data/data.db',
-  synchronize: false,
-  logging: false,
+  database: './data/data.db',　 // mysql, postgresqlはデータベース名
+
+  // 2.Entityクラス
   entities: [
-    // 1.auth
+    // auth
     LoginUser,
     LoginUserRole,
     Role
 
-    // 2.master
+    // master
 
-    // 3.transaction
+    // transaction
   ],
+
+  // 3.マイグレーションファイルの格納場所
   migrations: ['./migrations/*.ts'],
-  subscribers: []
+
+  // 4.?
+  subscribers: [],
+
+  // 5.ログを出力するか
+  logging: true,
+
+  // 6.trueにするとテーブルが自動生成される、本番環境では使用禁止
+  synchronize: false
 })
