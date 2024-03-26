695ff2f91242520b541b3597472d6a10852d91ab
mk-github-1 <32920703+mk-github-1@users.noreply.github.com>
2024-03-26 12:27:38 +0900

    Update ILoginUserRepository.ts

--- backend/src/domain/auth/LoginUser/ILoginUserRepository.ts
+++ backend/src/domain/auth/LoginUser/ILoginUserRepository.ts
@@ -3,14 +3,9 @@ import { LoginUserDto } from '@/application/auth/LoginUser/LoginUserDto'
 
 export interface ILoginUserRepository extends IGenericRepository<LoginUserDto> {
   find(): Promise<LoginUserDto[]>
-
   findOne(keys: string[]): Promise<LoginUserDto>
-
   create(loginUserDto: LoginUserDto): Promise<LoginUserDto>
-
   update(keys: string[], loginUserDto: LoginUserDto): Promise<LoginUserDto>
-
-  delete(keys: string[]): Promise<string>
-
+  delete(keys: string[]): Promise<string[]>
   sort<T extends { key: string; value: number }>(lists: T[]): Promise<number>
 }
